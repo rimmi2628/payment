f95ab4ccd36c9176ab67305c1c58640183b3be91
rimmi <rimzimranga@gmail.com>
2023-05-08 18:19:18 +0530

    new comit

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require('express')
 const bodyparser = require('body-parser')
 const path = require('path')
 const app = express()
- 
+ const model=require('./models');
+ const Customer=model.Stripe;
 
-const publishable_key='pk_test_51BTUDGJAJfZb9HEBwDg86TN1KNprHjkfipXmEDMb0gSCassK5T3ZfxsAbcgKVmAIXF7oZ6ItlZZbXO6idTHE67IM007EwQ4uN3';
-const secret_key='sk_test_tR3PYbcVNZZ796tH88S4VQ2u';
+const publishable_key='pk_test_51N5NDcSJkVRa4K2hy306krOmNTChXdNLnYCwICk5BvOWQTvZWbykMvsrvfMDb7HCLTNFrv9N7fbepl2WIqQxcs9q00ST9yGHDy';
+const secret_key='sk_test_51N5NDcSJkVRa4K2hnXRbAMawNsUA0jf7eXfroe3qfySxOd92Ead0Pp9T4nj27pspX7KufdqzMrjw84AshI9jEmz800doF75vs8';
  
 const stripe = require('stripe')(secret_key)
  
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 2000
  
 app.use(bodyparser.urlencoded({extended:false}))
 app.use(bodyparser.json())
@@ -18,46 +19,130 @@ app.use(bodyparser.json())
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
  
-app.get('/', function(req, res){
-res.render('Home', {
-key:publishable_key
-})
-});
+// app.get('/', function(req, res){
+// res.render('Home', {
+// key:publishable_key
+// })
+// });
 
-app.post('/payment', function(req, res){
+// app.post('/payment', function(req, res){
  
-    // Moreover you can take more details from user
-    // like Address, Name, etc from form
-    stripe.customers.create({
-    email: req.body.stripeEmail,
-    source: req.body.stripeToken,
-    name: 'Rimzim',
-    address: {
-    line1: 'TC 9/4 Old MES colony',
-    postal_code: '110092',
-    city: 'New Delhi',
-    state: 'Delhi',
-    country: 'India',
-    }
-    })
-    .then((customer) => {
+//     // Moreover you can take more details from user
+//     // like Address, Name, etc from form
+//     stripe.customers.create({
+//     email: req.body.stripeEmail,
+//     source: req.body.stripeToken,
+//     name: 'Rimzim',
+//     address: {
+//     line1: 'TC 9/4 Old MES colony',
+//     postal_code: '110092',
+//     city: 'New Delhi',
+//     state: 'Delhi',
+//     country: 'India',
+//     }
+//     })
+//     .then((customer) => {
      
-    return stripe.charges.create({
-    amount: 7000, // Charing Rs 25
-    description: 'Web Development Product',
-    currency: 'INR',
-    customer: customer.id
-    });
-    })
-    .then((charge) => {
-    res.send("Success") // If no error occurs
-    })
-    .catch((err) => {
-        console.log(err);
-    res.send(err) // If some error occurs
-    });
-    })
- 
+//     return stripe.charges.create({
+//     amount: 7000, // Charing Rs 25
+//     description: 'Web Development Product',
+//     currency: 'INR',
+//     customer: customer.id
+//     });
+//     })
+//     .then((charge) => {
+//     res.send("Success") // If no error occurs
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     res.send(err) // If some error occurs
+//     });
+//     })
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.render('index', { message: null, currentYear: new Date().getFullYear() ,key:publishable_key,req});
+  });
+
+
+  app.post('/payment', async (req, res) => {
+    const { email, cardNumber, expMonth, expYear, cvc } = req.body;
+  
+    try {
+      let customer = await Customer.findOne({
+        where: { email },
+      });
+  
+      if (!customer) {
+        // Step 1: Create a new customer in Stripe
+        const stripeCustomer = await stripe.customers.create({
+          email,
+        });
+  
+        // Step 2: Store the customer in the database
+        customer = await Customer.create({
+          email,
+          stripe_id: stripeCustomer.id,
+        });
+      }
+  
+      // Step 3: Retrieve customer data from Stripe
+      const stripeCustomer = await stripe.customers.retrieve(customer.stripe_id);
+    console.log("stripecustomers.....",stripeCustomer)
+      // Step 4: Check if the customer already has the new card
+      // const existingCards =  stripeCustomer.sources ? stripeCustomer.sources.data : [];
+      // console.log("existingCards",existingCards);
+      // const cardExists = existingCards.some(card => card.last4 === cardNumber.substr(-4));
+      // console.log("cardExists",cardExists);
+
+  
+      // if (cardExists) {
+      //   const message = 'Card already exists for the customer.';
+      //   return res.status(400).json({ message });
+      // }
+
+      const token = await stripe.tokens.create({
+        card: {
+          number: cardNumber,
+          exp_month:expMonth ,
+          exp_year: expYear,
+          cvc: cvc,
+        },
+      });
+      console.log("token,,,,,",token)
+      const existingCards =  token.card? token.card: [];
+      console.log("existingCards",existingCards);
+      
+      const cardExists = (existingCards.last4 === cardNumber.substr(-4));
+      console.log("cardExists",cardExists);
+
+
+      if (cardExists) {
+        const message = 'Card already exists for the customer.';
+        return res.status(400).json({ message });
+      }
+
+
+      // Step 5: Create a new card for the customer in Stripe
+      await stripe.customers.createSource(customer.stripe_id, {
+        source:	'tok_visa'
+       
+        
+        
+      });
+  
+      const message = 'New card added to the customer.';
+      return res.status(200).json({ message });
+    } catch (error) {
+      console.error('Error:', error);
+      return res.status(500).json({ error });
+    }
+  });
+  
 app.listen(port, function(error){
 if(error) throw error
 console.log("Server created Successfully")
